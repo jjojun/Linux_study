fcca3f514c241d8c5200c53725c80866dd9625a4
jjojun <dmddkslek0@gmail.com>
2024-06-04 12:26:15 +0900

    0604

---
+++ 0604/ex10-07.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void timeover (int signum){
+    printf("\n\ntime over!!\n\n");
+    exit(0);
+}
+
+int main(){
+    char buf[1024];
+    char *alpha = "abcdefghijklmnopqrstuvwxyz";
+
+    int timelimit;
+    struct sigaction act;
+
+    act.sa_handler = timeover;
+    sigaction(SIGALRM, &act, NULL);
+    printf("input timelimit (sec)..\n");
+    scanf("%d", &timelimit);
+    alarm(timelimit);
+    printf("START!!\n > ");
+    scanf("%s", buf);
+    if(!strcmp(buf, alpha))
+	printf("well done.. you succeedd!\n");
+    else
+	printf("sorry.. you fail!\n");
+}
